2009dac8bed97b3e12928e29766f6530b27b661c
Jan Jelschen <jan.jelschen@itemis.de>
2018-08-16 16:53:50 +0200

    add a tree viewer to demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TreeViewerModule } from './modules/widgets/tree-viewer/tree-viewer.module';
 
 
 @NgModule({
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, TreeViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
