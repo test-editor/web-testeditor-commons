4ad9b944a840786ceab1cdc065482f54a94cf695
Jan Jelschen <jan.jelschen@itemis.de>
2018-08-16 16:53:50 +0200

    add a tree viewer to demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TreeNode, CommonTreeNodeActions } from './modules/widgets/tree-viewer/tree-node';
+import { TreeViewerConfig } from './modules/widgets/tree-viewer/tree-viewer-config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  model = new TreeNode({
+    name: 'parent node',
+    collapsedCssClasses: 'fa-chevron-right',
+    expandedCssClasses: 'fa-chevron-down',
+    leafCssClasses: 'fa-folder',
+    expanded: false,
+    children: [
+      { name: 'child node 1', children: [] },
+      { name: 'child node 2', children: [] },
+      { name: 'child node 3', children: [] }
+    ]
+  });
+
+  treeConfig: TreeViewerConfig = {
+    onDoubleClick: (node: TreeNode) => node.expanded = !node.expanded,
+    onIconClick: (node: TreeNode) => node.expanded = !node.expanded,
+    onClick: (node: TreeNode) => node.selectOnly(),
+    onKeyPress: CommonTreeNodeActions.arrowKeyNavigation
+  };
 }
