c695e2cc810aba4e66ea2a2e33108a98719c0ec4
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-11 15:53:02 +0200

    migrate and generalize NewElementCmp

---
+++ src/app/modules/widgets/tree-viewer/new-element/new-element.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild, Input, ElementRef, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import { TreeNode } from '../tree-node';
+
+export interface NewElementConfig {
+  indent: string;
+  parentNode: TreeNode;
+  validateName: (newName: string) => { valid: boolean, message?: string };
+  createNewElement: (parentNode: TreeNode, newName: string) => boolean;
+  iconCssClasses: string;
+}
+
+
+@Component({
+  selector: 'app-new-element',
+  templateUrl: './new-element.component.html',
+  styleUrls: ['./new-element.component.css']
+})
+export class NewElementComponent implements AfterViewInit {
+
+  @ViewChild('theInput') input: ElementRef;
+  @Input() config: NewElementConfig;
+  @Output() cancelled = new EventEmitter<void>();
+  @Output() succeeded = new EventEmitter<void>();
+
+  errorMessage: string;
+
+  ngAfterViewInit(): void {
+    this.input.nativeElement.focus();
+  }
+
+  onKeyup(event: any): void {
+    this.validate();
+  }
+
+  validate(): boolean {
+    const newName: string = this.input.nativeElement.value;
+    const nameCheckResult = this.config.validateName(newName);
+    this.errorMessage = nameCheckResult.valid ? null : nameCheckResult.message;
+    return nameCheckResult.valid;
+  }
+
+  onEnter(): void {
+    if (this.validate()) {
+      if (this.config.createNewElement(this.config.parentNode, this.input.nativeElement.value)) {
+        this.succeeded.emit();
+      } else {
+        this.errorMessage = 'Error while creating element!';
+      }
+    }
+  }
+
+  onCancel(): void {
+    this.cancelled.emit();
+  }
+
+}
--- src/app/modules/widgets/tree-viewer/tree-viewer.module.ts
+++ src/app/modules/widgets/tree-viewer/tree-viewer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TreeViewerComponent } from './tree-viewer.component';
+import { NewElementComponent } from './new-element/new-element.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    TreeViewerComponent
+    TreeViewerComponent,
+    NewElementComponent
   ],
   exports: [
     TreeViewerComponent
