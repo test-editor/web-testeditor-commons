fda39244758a84b00af05152650f32f2b6e1c047
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-12 17:50:40 +0200

    add types to public api

--- public_api.ts
+++ public_api.ts
@@ -1,6 +1,10 @@
 export * from './src/app/modules/widgets/tree-viewer/tree-viewer.module';
 export * from './src/app/modules/widgets/tree-viewer/tree-node';
 export * from './src/app/modules/widgets/tree-viewer/tree-viewer-config';
+export { NewElementConfig } from './src/app/modules/widgets/tree-viewer/new-element/new-element.component';
 export * from './src/app/modules/services/browser-object-model/window.service';
 export * from './src/app/modules/services/browser-object-model/default.window.service';
 export * from './src/app/modules/event-types-out';
+export * from './src/app/modules/event-types-in';
+export * from './src/app/modules/widgets/tree-viewer/tree-viewer-embedded-button';
+export * from './src/app/modules/widgets/tree-viewer/confirmation-needing-action';
