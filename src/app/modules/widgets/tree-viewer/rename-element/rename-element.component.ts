712e449281aefb2a0d22774039627d8de81b1b37
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-12 10:13:37 +0200

    migrate RenameElementComponent from workspace nav

--- src/app/modules/widgets/tree-viewer/rename-element/rename-element.component.ts
+++ src/app/modules/widgets/tree-viewer/rename-element/rename-element.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, Input, ElementRef, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import { MessagingService } from '@testeditor/messaging-service';
+import { TreeNode } from '../tree-node';
+
+export interface RenameElementConfig {
+  element: TreeNode;
+  validateName: (newName: string) => { valid: boolean, message?: string };
+  renameElement: (Element, newName: string) => boolean;
+}
 
 @Component({
   selector: 'app-rename-element',
   templateUrl: './rename-element.component.html',
   styleUrls: ['./rename-element.component.css']
 })
-export class RenameElementComponent implements OnInit {
+export class RenameElementComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('renameInput') input: ElementRef;
+  @Input() config: RenameElementConfig;
+  @Output() cancelled = new EventEmitter<void>();
+  @Output() succeeded = new EventEmitter<void>();
 
-  ngOnInit() {
+  errorMessage: string;
+
+  ngAfterViewInit(): void {
+    this.input.nativeElement.focus();
+  }
+
+  onKeyup(event: KeyboardEvent): void {
+    this.validate();
+    event.stopPropagation();
+  }
+
+  validate(): boolean {
+    const newName: string = this.input.nativeElement.value;
+    const nameCheckResult = this.config.validateName(newName);
+    this.errorMessage = nameCheckResult.valid ? null : nameCheckResult.message;
+    return nameCheckResult.valid;
+  }
+
+  onEnter(): void {
+    if (this.validate()) {
+      if (this.config.renameElement(this.config.element, this.input.nativeElement.value)) {
+        this.succeeded.emit();
+      } else {
+        this.errorMessage = 'Error while creating element!';
+      }
+    }
+  }
+
+  onCancel(): void {
+    this.cancelled.emit();
   }
 
 }
