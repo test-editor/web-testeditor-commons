2aed5501700d18727cac6f727026ed197cf69e40
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-09-13 08:22:17 +0200

    add event types in

---
+++ src/app/modules/event-types-in.ts
@@ -0,0 +1,4 @@
+// all events this components subscribes to for other components to publish
+
+// payload { node: TreeNode, root: TreeNode }
+export const TREE_NODE_CREATE_AT_SELECTED = 'treenode.create-at-selected';
