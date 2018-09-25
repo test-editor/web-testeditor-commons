ac8e27727d29de5ad82b55bff103730a093d6757
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-25 14:31:22 +0200

    export marker field via public API

--- public_api.ts
+++ public_api.ts
@@ -7,3 +7,4 @@ export * from './src/app/modules/event-types-out';
 export * from './src/app/modules/event-types-in';
 export * from './src/app/modules/widgets/tree-viewer/tree-viewer-embedded-button';
 export * from './src/app/modules/widgets/tree-viewer/confirmation-needing-action';
+export * from './src/app/modules/widgets/tree-viewer/markers/field';
