9fd76d197c77707dbf867cee81f7916c7c345de5
Jan Jelschen <jan.jelschen@itemis.de>
2019-02-05 14:36:44 +0100

    use keydown instead of keyup to prevent default scrolling

--- src/app/modules/widgets/tree-viewer/tree-viewer-keyboard-decorator.component.ts
+++ src/app/modules/widgets/tree-viewer/tree-viewer-keyboard-decorator.component.ts
@@ -33,7 +33,7 @@ export class TreeViewerKeyboardDecoratorComponent implements OnInit, OnDestroy {
     }
   }
 
-  onKeyUp(event: KeyboardEvent) {
+  onKeyDown(event: KeyboardEvent) {
     if (this.config.onKeyPress && this.config.onKeyPress.has(event.key)) {
       this.config.onKeyPress.get(event.key)(this.selectedNode);
       event.preventDefault();
